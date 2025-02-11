cefa4892c873f73b02e6c0e5ea13d715e019acda
azadov-azamat <azamatazadov112@gmail.com>
2025-02-11 13:11:25 +0500

    changes

--- components/content-loader/index.ts
+++ components/content-loader/index.ts
@@ -2,4 +2,4 @@ export {default as ContentLoaderTopSearches} from './top-searches';
 export {default as ContentLoaderLoadGrid} from './load-grid';
 export {default as ContentLoaderLoadList} from './load-list';
 export {default as ContentPaymentPageLoader} from './payment';
-export {default as ContentSubscriptionLoader} from './subscriptions';
+export {default as ContentSubscriptionLoader} from './load-grid copy';
