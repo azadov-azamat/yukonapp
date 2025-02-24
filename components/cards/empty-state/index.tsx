6c6fdac9a600a48d1bf83175581d54a8a069ac98
azadov-azamat <azamatazadov112@gmail.com>
2025-02-24 19:31:09 +0500

    changes

--- components/cards/empty-state/index.tsx
+++ components/cards/empty-state/index.tsx
@@ -8,7 +8,7 @@ const EmptyState: React.FC<{type: string}> = ({ type }) => {
     return (
         <View className="p-6 border rounded-lg shadow-md border-border-color/20 bg-primary-light dark:bg-primary-dark/20">
             <Text className="mb-2 text-lg font-bold text-center text-primary-title-color dark:text-white">{t ('not-found.' + type + '.title')}</Text>
-            <Text className="text-sm text-center text-primary-desc-color dark:text-border-color/20">{t ('not-found.' + type + '.description')}</Text>
+            <Text className="text-sm text-center text-primary-desc-color dark:text-border-color">{t ('not-found.' + type + '.description')}</Text>
         </View>
     );
 };
